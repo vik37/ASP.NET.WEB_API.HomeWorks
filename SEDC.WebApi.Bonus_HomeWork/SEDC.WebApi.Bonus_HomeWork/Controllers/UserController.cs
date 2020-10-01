8b8a00f4f271418836828f4216f0a82f41a09930
vik37 <59177404+vik37@users.noreply.github.com>
2020-10-01 23:06:26 +0200

    WebApi_Bonus_HomeWork1

---
+++ SEDC.WebApi.Bonus_HomeWork/SEDC.WebApi.Bonus_HomeWork/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SEDC.WebApi.Bonus_HomeWork.Model;
+
+namespace SEDC.WebApi.Bonus_HomeWork.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<User>> GetUsers()
+        {
+            return Ok(StaticDb.AllUsers);
+        }
+
+        [HttpPost]
+        public ActionResult CreateUser()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(Request.Body))
+                {
+                    var jsonBody = sr.ReadToEnd();
+                    var user = JsonConvert.DeserializeObject<User>(jsonBody);
+                    StaticDb.AllUsers.Add(user);
+                    return StatusCode(StatusCodes.Status201Created, "User successfully created!");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
---
+++ SEDC.WebApi.Bonus_HomeWork/SEDC.WebApi.Bonus_HomeWork/StaticDb.cs
@@ -0,0 +1,57 @@
+﻿using SEDC.WebApi.Bonus_HomeWork.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.WebApi.Bonus_HomeWork
+{
+    public static class StaticDb
+    {
+        public static List<User> AllUsers = new List<User>()
+        {
+            new User()
+            {
+                Id = 1,
+                FirstName = "Viktor",
+                LastName = "Zafirovski"
+            },
+            new User()
+            {
+                Id = 2,
+                FirstName = "Goce",
+                LastName = "Kabov"
+            },
+            new User()
+            {
+                Id = 3,
+                FirstName = "Darko",
+                LastName = "Pancevski"
+            },
+            new User()
+            {
+                Id = 4,
+                FirstName = "Marija",
+                LastName = "Kiri"
+            },
+            new User()
+            {
+                Id = 5,
+                FirstName = "Petre",
+                LastName = "M.Adreevski"
+            },
+            new User()
+            {
+                Id = 6,
+                FirstName = "Andrea",
+                LastName = "Lekic"
+            },
+            new User()
+            {
+                Id = 7,
+                FirstName = "Saban",
+                LastName = "Trstena"
+            }
+        };
+    }
+}
